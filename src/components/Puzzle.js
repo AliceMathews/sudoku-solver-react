a9a671481688aa0b314cae08dd3748222640d905
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-05 18:59:43 -0800

    building up puzzle components layout

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+import "./Puzzle.css";
+import Box from "./Box";
+
 export default function Puzzle() {
-  return <div>puzzle container</div>;
+  return (
+    <div className="container">
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+    </div>
+  );
 }
