73140f1ab66a1a027c2badbb241f5ff382c9e6c3
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-05 18:24:10 -0800

    defined puzzle and individual box components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import Puzzle from "./components/Puzzle";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Sudoku Solver</p>
+        <Puzzle />
       </header>
     </div>
   );
