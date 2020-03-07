b31272eb1676a1a764b81c2f2d98d1b3bd9b9847
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-07 14:32:56 -0800

    added text input for each cell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Cell() {
-  return <div className="cell" />;
+  return (
+    <div className="cell">
+      <form>
+        <input type="text" name="number" placeholder="4" />
+      </form>
+    </div>
+  );
 }
