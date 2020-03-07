8c415d3d4fcafd987b4b6122cba41b58f1a2e1d5
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-07 15:56:01 -0800

    gave each box an id, used to calc array coords

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Puzzle.css";
 import Box from "./Box";
 
 export default function Puzzle() {
+  const [gameState, setGameState] = useState([
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""]
+  ]);
+
   return (
     <div className="container">
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
+      <Box id={[0, 0]} addValue={setGameState} />
+      <Box id={[0, 1]} addValue={setGameState} />
+      <Box id={[0, 2]} addValue={setGameState} />
+      <Box id={[1, 0]} addValue={setGameState} />
+      <Box id={[1, 1]} addValue={setGameState} />
+      <Box id={[1, 2]} addValue={setGameState} />
+      <Box id={[2, 0]} addValue={setGameState} />
+      <Box id={[2, 1]} addValue={setGameState} />
+      <Box id={[2, 2]} addValue={setGameState} />
     </div>
   );
 }
