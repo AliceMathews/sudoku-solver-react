d2fc691548951b8c1938056ab27b5071deedcff2
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-07 14:52:40 -0800

    removed placeholder for text input

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,13 +3,7 @@ import React from "react";
 export default function Cell() {
   return (
     <div className="cell">
-      <input
-        className="number-input"
-        type="text"
-        name="number"
-        placeholder="4"
-        maxLength="1"
-      />
+      <input className="number-input" type="text" name="number" maxLength="1" />
     </div>
   );
 }
