292284aa1871b4c4469e3c7299d179d1fdfb79ba
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-07 15:56:40 -0800

    added props to each cell for row and column coords, calculated oof box coords

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,18 +1,54 @@
 import React from "react";
 import Cell from "./Cell";
 
-export default function Box() {
+export default function Box(props) {
   return (
     <div className="box">
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
+      <Cell
+        row={3 * props.id[0]}
+        column={3 * props.id[1]}
+        addValue={props.addValue}
+      />
+      <Cell
+        row={3 * props.id[0]}
+        column={3 * props.id[1] + 1}
+        addValue={props.addValue}
+      />
+      <Cell
+        row={3 * props.id[0]}
+        column={3 * props.id[1] + 2}
+        addValue={props.addValue}
+      />
+      <Cell
+        row={3 * props.id[0] + 1}
+        column={3 * props.id[1]}
+        addValue={props.addValue}
+      />
+      <Cell
+        row={3 * props.id[0] + 1}
+        column={3 * props.id[1] + 1}
+        addValue={props.addValue}
+      />
+      <Cell
+        row={3 * props.id[0] + 1}
+        column={3 * props.id[1] + 2}
+        addValue={props.addValue}
+      />
+      <Cell
+        row={3 * props.id[0] + 2}
+        column={3 * props.id[1]}
+        addValue={props.addValue}
+      />
+      <Cell
+        row={3 * props.id[0] + 2}
+        column={3 * props.id[1] + 1}
+        addValue={props.addValue}
+      />
+      <Cell
+        row={3 * props.id[0] + 2}
+        column={3 * props.id[1] + 2}
+        addValue={props.addValue}
+      />
     </div>
   );
 }
