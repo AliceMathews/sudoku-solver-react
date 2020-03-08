493064588144f7ee9e8ebabc504ad5111ef2705f
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-07 17:16:36 -0800

    updated cell to change state & WIP update state function

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function Cell() {
+export default function Cell(props) {
   return (
     <div className="cell">
-      <input className="number-input" type="text" name="number" maxLength="1" />
+      <input
+        className="number-input"
+        type="text"
+        name="number"
+        value={props.value}
+        onChange={value => props.addValue([props.row, props.column, value])}
+        maxLength="1"
+      />
     </div>
   );
 }
--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -16,17 +16,21 @@ export default function Puzzle() {
     ["", "", "", "", "", "", "", "", ""]
   ]);
 
+  const updateGame = function(row, column, value) {
+    // setGameState(prevGameState => [...gameState, ])
+  };
+
   return (
     <div className="container">
-      <Box id={[0, 0]} addValue={setGameState} />
-      <Box id={[0, 1]} addValue={setGameState} />
-      <Box id={[0, 2]} addValue={setGameState} />
-      <Box id={[1, 0]} addValue={setGameState} />
-      <Box id={[1, 1]} addValue={setGameState} />
-      <Box id={[1, 2]} addValue={setGameState} />
-      <Box id={[2, 0]} addValue={setGameState} />
-      <Box id={[2, 1]} addValue={setGameState} />
-      <Box id={[2, 2]} addValue={setGameState} />
+      <Box id={[0, 0]} addValue={updateGame} />
+      <Box id={[0, 1]} addValue={updateGame} />
+      <Box id={[0, 2]} addValue={updateGame} />
+      <Box id={[1, 0]} addValue={updateGame} />
+      <Box id={[1, 1]} addValue={updateGame} />
+      <Box id={[1, 2]} addValue={updateGame} />
+      <Box id={[2, 0]} addValue={updateGame} />
+      <Box id={[2, 1]} addValue={updateGame} />
+      <Box id={[2, 2]} addValue={updateGame} />
     </div>
   );
 }
