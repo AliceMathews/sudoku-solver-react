d9a82472a82c21dfa048a926e3bef9766298c437
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-11 18:40:42 -0700

    value rendered when updated

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 export default function Cell(props) {
+  const row = props.row;
+  const column = props.column;
+  const value = props.values[row][column];
+  console.log(value);
+  console.log(props.row);
+  console.log(props.column);
+
   return (
     <div className="cell">
       <input
         className="number-input"
         type="text"
         name="number"
-        value={props.value}
+        value={value}
         onChange={value => props.addValue([props.row, props.column, value])}
         maxLength="1"
       />
