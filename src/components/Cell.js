38e02798bd92e4b1a7c5e69dc3704d741151ddb3
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-11 21:47:59 -0700

    changed format of data passed when updating puzzle state

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,9 +4,9 @@ export default function Cell(props) {
   const row = props.row;
   const column = props.column;
   const value = props.values[row][column];
-  console.log(value);
-  console.log(props.row);
-  console.log(props.column);
+  // console.log(value);
+  // console.log(props.row);
+  // console.log(props.column);
 
   return (
     <div className="cell">
@@ -15,7 +15,9 @@ export default function Cell(props) {
         type="text"
         name="number"
         value={value}
-        onChange={value => props.addValue([props.row, props.column, value])}
+        onChange={event =>
+          props.addValue(props.row, props.column, event.target.value)
+        }
         maxLength="1"
       />
     </div>
