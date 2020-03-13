b9f280dc65f06a76977f9cfc6e17232760b5d3b4
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-12 21:03:56 -0700

    finds coords of matching value

--- src/helpers/solver.js
+++ src/helpers/solver.js
@@ -22,5 +22,30 @@ const checkRows = function(row, col, val, gameState) {
     rowArr = gameState.slice(6, 9);
   }
 
-  console.log(rowArr);
+  const numOfOccurances = rowArr.reduce((total, currentVal) => {
+    if (currentVal.includes(val)) {
+      return total + 1;
+    } else return total;
+  }, 0);
+
+  if (numOfOccurances === 2) {
+    let matchRow = undefined;
+    let matchCol = undefined;
+
+    if (row % 3 === 0) {
+      if (rowArr[1].includes(val)) {
+        matchRow = row + 1;
+        matchCol = rowArr[1].findIndex(el => el === val);
+      } else {
+        matchRow = row + 2;
+        matchCol = rowArr[2].findIndex(el => el === val);
+      }
+
+      console.log(matchRow, matchCol);
+    } else if (row % 3 === 1 || row === 1) {
+      matchRow = row + 1;
+      matchCol = rowArr[2].findIndex(el => el === val);
+      console.log(matchRow, matchCol);
+    }
+  }
 };
