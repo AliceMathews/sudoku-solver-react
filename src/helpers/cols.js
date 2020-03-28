fb6b21a06da473251061ff8c4868ff3defe90470
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-27 17:42:12 -0700

    calculates 2nd match coords and passes into find 3rd val func

--- src/helpers/cols.js
+++ src/helpers/cols.js
@@ -28,71 +28,40 @@ export default function checkCols(row, col, val, game) {
 
   console.log("val: ", val, "num Occur: ", numOfOccurances);
 
-  /*
   if (numOfOccurances === 2) {
     let matchRow = undefined;
     let matchCol = undefined;
 
-    if (row % 3 === 0) {
-      if (rowArr[1].includes(val)) {
-        matchRow = row + 1;
-        matchCol = rowArr[1].findIndex(el => el === val);
-      } else {
-        matchRow = row + 2;
-        matchCol = rowArr[2].findIndex(el => el === val);
+    if (col % 3 === 0) {
+      for (const [i, row] of colArr.entries()) {
+        if (row[1] === val) {
+          matchRow = i;
+          matchCol = col + 1;
+        } else if (row[2] === val) {
+          matchRow = i;
+          matchCol = col + 2;
+        }
       }
-    } else if (row % 3 === 1 || row === 1) {
-      matchRow = row + 1;
-      matchCol = rowArr[2].findIndex(el => el === val);
-    }
+    } else if (col % 3 === 1 || col === 1) {
+      matchCol = col + 1;
 
-    if (matchCol > 0) {
+      for (const [i, row] of colArr.entries()) {
+        if (row[2] === val) {
+          matchRow = i;
+        }
+      }
+    }
+    console.log("match row: ", matchRow, "match col: ", matchCol);
+
+    if (matchCol && matchRow) {
       return find3rdVal(val, block, [row, col], [matchRow, matchCol], game);
     }
   }
 }
 
 const find3rdVal = function(val, block, coords1, coords2, game) {
-  // console.log("val: ", val);
-  // console.log("coords1: ", coords1);
-  // console.log("coords2: ", coords2);
-  // console.log("block: ", block);
-
-  const missingRow = blockLookup[block].find(
-    el => el !== coords1[0] && el !== coords2[0]
-  );
-
-  const missingColBlock = Object.keys(blockLookup).find(
-    key =>
-      !blockLookup[key].includes(coords1[1]) &&
-      !blockLookup[key].includes(coords2[1])
-  );
-
-  let potentialCols = blockLookup[missingColBlock];
-  // console.log("pot cols 1", potentialCols);
-
-  //filter out any cells that are already filled
-  potentialCols = potentialCols.filter(col => game[missingRow][col] === "");
-  // console.log("pot cols 2", potentialCols);
-
-  //filter out any cells that the cols already contain the val
-  potentialCols = potentialCols.filter(col => {
-    let valFound = 0;
-    for (let i = 0; i < 9; i++) {
-      if (game[i][col] === val) {
-        valFound = 1;
-      }
-    }
-    if (valFound === 0) return String(col); //convert to string to deal with bug where if col is 0, you get an empty array
-    return undefined;
-  });
-  // console.log("pot cols 3", potentialCols);
-
-  if (potentialCols.length === 1) {
-    const missingCol = potentialCols[0];
-
-    const updatedGame = updateGame(missingRow, missingCol, val, game);
-    return { updatedGame }; //updates the rowUpdated value
-  }
- */
-}
+  console.log("val: ", val);
+  console.log("coords1: ", coords1);
+  console.log("coords2: ", coords2);
+  console.log("block: ", block);
+};
